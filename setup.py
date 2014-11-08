f7991f870b09d3d0f2a6f0cdd2395967bc20ecf6
float1251 <taka.05022002@gmail.com>
2014-11-08 22:58:39 +0900

    setup.pyの修正

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+
+with open("README.rst") as f:
+    readme = f.read()
+
 setup(
     name="flmakesetup",
     version="0.0.1",
     author="takahrio iwatani",
     author_email="taka.05022002@gmail.com",
     description="easy create setup.py",
-    url="http",
+    long_description=readme,
+    url="https://github.com/float1251/flmakesetup",
     py_modules=["autosetup"],
     entry_points={
         "console_scripts":[
