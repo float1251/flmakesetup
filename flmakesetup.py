b0ab771aa9b1f0d188b04ca6f2532c2cee861991
float1251 <taka.05022002@gmail.com>
2014-11-10 17:24:33 +0900

    add setup test-env

--- flmakesetup.py
+++ flmakesetup.py
@@ -1,7 +1,10 @@
-import os, sys
+import os
+import sys
+import subprocess
 from argparse import ArgumentParser
 import urllib.request as req
 from jinja2 import Environment, PackageLoader
+import pip
 
 username = os.getlogin()
 path = os.getcwd()
@@ -48,12 +51,19 @@ def create_setuppy(args):
     conf = {}
     conf["name"] = _input_required_value("name:")
     conf["version"] = _input_required_value("version:")
-    conf["author"] = _input_required_value("author(default={}):".format(username), default=username)
+    conf["author"] = _input_required_value(
+        "author(default={}):".format(username),
+        default=username
+    )
     conf["author_email"] = _input_optional_value("author_email:")
     conf["test_suite"] = _input_optional_value("test_suite:")
 
     # 空白や改行の設定をする
-    env = Environment(loader=PackageLoader("flmakesetup"), trim_blocks=True, lstrip_blocks=True)
+    env = Environment(
+        loader=PackageLoader("flmakesetup"),
+        trim_blocks=True,
+        lstrip_blocks=True
+    )
     template = env.get_template("setup.tmpl")
     print(template.render(**conf))
     # fileをexportする
@@ -65,11 +75,23 @@ def create_setuppy(args):
     file = open(p, mode="w")
     file.write(template.render(**conf))
 
+
 def setup_test_environment(args):
     """
     testに必要なライブラリ等をinstallする.
     """
-    pass
+    # coverageをinstallする
+    dist_list = pip.get_installed_distributions()
+    if not any(["coverage" in dist.project_name for dist in dist_list]):
+        subprocess.call("pip install coverage", shell=True)
+    else:
+        print("Already install coverage")
+
+    # flake8をinstallする
+    if not any(["flake8" in dist.project_name for dist in dist_list]):
+        subprocess.call("pip install flake8", shell=True)
+    else:
+        print("Already install flake8")
 
 
 def main():
@@ -82,12 +104,18 @@ def main():
     create_parser.set_defaults(func=create_setuppy)
 
     # setttings show-classifiers
-    show_parser = sub_parser.add_parser("show-classifiers", help="show classifiers list")
+    show_parser = sub_parser.add_parser(
+        "show-classifiers",
+        help="show classifiers list"
+    )
     show_parser.set_defaults(func=show_classifiers)
 
     # setup test-env
-    #test_env_parser = sub_parser.add_parser("setup-test-env", help="setup test environment")
-    #test_env_parser.set_defaults(func=setup_test_environment)
+    test_env_parser = sub_parser.add_parser(
+        "setup-test-env",
+        help="setup test environment"
+    )
+    test_env_parser.set_defaults(func=setup_test_environment)
 
     args = parser.parse_args()
 
